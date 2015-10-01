24f694019c4363c4a41314cd2ddbbc63accd9040
Esyir <sakan1000@gmail.com>
2015-10-01 11:15:11 +0800

    resolving conflicts

--- Victor/JobOfferDescription.php
+++ Victor/JobOfferDescription.php
@@ -1,5 +1,4 @@
 <html>
-<<<<<<< Updated upstream
 <head> <title>Job Offer Description</title> 
 <link rel="stylesheet" href="CSS/styles.css">
 <!-- Latest compiled and minified CSS -->
@@ -11,18 +10,10 @@
 </head>
 
 <body bgcolor="#D8D8D8">
-
-<?php
-putenv('ORACLE_HOME=/oraclient');
-$dbh = ocilogon('a0110801', 'crse1510', '(DESCRIPTION =
-=======
-<head> <title>Job Description</title> </head>
 <h1> Job Description</h1>
-<body>
 <?php
 putenv('ORACLE_HOME=/oraclient');
 $dbh = ocilogon('e0009809', 'crse1510', '(DESCRIPTION =
->>>>>>> Stashed changes
 	(ADDRESS_LIST =
 	 (ADDRESS = (PROTOCOL = TCP)(HOST = sid3.comp.nus.edu.sg)(PORT = 1521))
 	)
