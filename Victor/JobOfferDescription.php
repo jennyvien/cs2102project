f4c709616fd008c7f7a2edd2248bdea3a2056c2e
Esyir <sakan1000@gmail.com>
2015-10-01 11:24:33 +0800

    merged conflicts

--- Victor/JobOfferDescription.php
+++ Victor/JobOfferDescription.php
@@ -23,31 +23,6 @@ $dbh = ocilogon('e0009809', 'crse1510', '(DESCRIPTION =
   )');
 ?>
 <?php
-<<<<<<< Updated upstream
-{
-	$sql="SELECT * FROM joboffers order by jobnum";
-	$stid=oci_parse($dbh, $sql);
-	oci_execute($stid, OCI_DEFAULT);
-	while($row = oci_fetch_row($stid)) {
-		echo "<div class=\"row\">";
-		echo "<div class=\"row\">";
-		echo "<div class=\"col-xs-offset-4 col-xs-4\">";
-		echo "<h1>" .$row[2] . "</h1>";
-		echo "</div>";
-		echo "<div class=\"row\">";
-		echo "<h6>" .$row[1] . "</h6>";  
-		echo "<h6>" .$row[4] . "</h6>";  
-		echo "<h6>" .$row[8] . "</h6>";  
-		echo "<h6>" .$row[9] . "</h6>";  
-		echo "</div>";
-		echo "</div>";
-	}
-}
-?>
-
-<?php
-oci_close($dbh);
-=======
 	$job_title=$_GET['job_title'];
 	echo "Title: " . $job_title;
 	
@@ -68,7 +43,6 @@ oci_close($dbh);
 	$salary=$_GET['salary'];
 	echo "<br> Salary: $" . $salary . "/year";
 		
->>>>>>> Stashed changes
 ?>
 </body>
 </html>
